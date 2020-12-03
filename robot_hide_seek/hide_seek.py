6a2a44b3ee3220b66fd060862b331d8a324e6fed
Rekicho <up201604145@fe.up.pt>
2020-12-03 14:49:38 +0000

    Initial setup

---
+++ robot_hide_seek/hide_seek.py
@@ -0,0 +1,40 @@
+import rclpy
+from rclpy.node import Node
+from rclpy.qos import qos_profile_sensor_data
+
+from sensor_msgs.msg import LaserScan
+from geometry_msgs.msg import Twist
+
+class HideSeek(Node):
+    
+    def __init__(self):
+        super().__init__('hide_seek')
+        self.subscription = self.create_subscription(
+            LaserScan, 
+            '/scan', 
+            self.lidar_callback,
+            qos_profile_sensor_data)
+        self.publisher = self.create_publisher(
+            Twist,
+            '/cmd_vel',
+            10
+        )
+
+    def lidar_callback(self, msg):
+        vel = Twist()
+        vel.linear.x = 1.0
+        self.publisher.publish(vel)
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    hide_seek = HideSeek()
+
+    rclpy.spin(hide_seek)
+
+    hide_seek.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
---
+++ setup.py
@@ -0,0 +1,30 @@
+import os
+from glob import glob
+from setuptools import setup
+
+package_name = 'robot_hide_seek'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+        (os.path.join('share', package_name, 'worlds'), glob('worlds/*.model')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='rekicho',
+    maintainer_email='rekicho@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'hide_seek = robot_hide_seek.hide_seek:main'
+        ],
+    },
+)
